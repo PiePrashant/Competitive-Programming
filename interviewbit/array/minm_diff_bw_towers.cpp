a2efb921201cc132fc7aa72c43448058533dd077
PiePrashant <prashantpie4321@gmail.com>
2019-05-29 23:35:25 +0530

    diff bw tower height done

--- interviewbit/array/minm_diff_bw_towers.cpp
+++ interviewbit/array/minm_diff_bw_towers.cpp
@@ -33,76 +33,49 @@ Output 2:
 */
 
 
-#include <bits/stdc++.h>
 #include <iostream>
-
+#include <bits/stdc++.h>
 using namespace std;
-int minm_diff(vector <int> &v, int c);
-
-
-int main() {
-    int test = 1;
-    while(test--) {
-        int n;
-        cin >> n;
-        vector <int> v(n);
-
-        for (int i = 0; i < n; i++) {
-            cin >> v[i];
-        }
-        int b;
-        cin >> b;
-
-        cout << minm_diff(v, b) << endl;
-    }
-    return 0;
-} 
-
-int minm_diff(vector <int> &A, int k) {
-    
-    sort(A.begin(), A.end());
-    int n = A.size();
-    int gap = A[n-1] - A[0] ;
-    
-    if (gap <= k) {
-        return gap;
-    }
-    
-    int global_small = min(A[0] + k, A[n-1] - k);
-    int global_big =max(A[0] + k, A[n-1] - k);
-    
-    for ( int i = 1; i < n - 1; i++) {
-
-        int local_small = A[i] - k;
-        int local_big = A[i] + k;
-
-        if (local_small > global_small) {
-            cout << '-' << " " << global_small<<" " << global_big << " " << endl;
-            continue;
-        }
-
-        if (local_big < global_big) {
-            cout << '+' << " " << global_small<<" " << global_big << " " << endl;
-            continue;
-        }
-
-        if (abs(local_big - global_small ) < abs(global_big - local_small ) {
-            global_big = local_big;
-            cout << '+' << " " << global_small<<" " << global_big << " " << endl;
-        }
-
-        else {
-            global_small = local_small;
-             cout << '-' << " " << global_small<<" " << global_big << " " << endl;
-        }
-        
-    }
-
-    return global_big - global_small;
-        
+int main()
+ {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int k,n;
+	    cin>>k>>n;
+	    int a[n];
+	    for(int i=0;i<n;i++)
+	    cin>>a[i];
+	   
+	    sort(a,a+n);
+	   
+	    int ans=a[n-1]-a[0];
+	    int small=a[0]+k;
+	    int big=a[n-1]-k;
+	    
+	    if(small>big)
+	    swap(small,big);
+	    
+	    
+	    
+	    for(int i=1;i<(n-1);i++)
+	    {
+	        int sub= a[i]-k;
+	        int add=a[i]+k;
+	        
+	        if(sub>=small||add<=big)
+	        continue;
+	        
+	        if(big-sub<=add-small)
+	         small=sub;
+	         else
+	         big=add;
+	     }
+	     cout<<min(ans,(big-small))<<endl;
+	}
+	return 0;
 }
-
-
 /* A : 4 6 7 10 12 16 22 29 40 41 46 57 67 72 75 75 76 81 85 86 87 89 92 95 97
 B : 88
 Your function returned the following :
